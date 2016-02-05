a066d3ceb79dc8c12f131199c32d97c782a1ec04
Alexoner <onerhao@gmail.com>
2016-02-05 20:53:47 +0800

    update softmax

--- build-doc.sh
+++ build-doc.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-    cd $(dirname $0); for i in ./prml/*; do echo "Convert $i"; ./articles/bin/mkhtml5 "$i" ;done; cd -
+    cd $(dirname "$0") || exit 1; for i in ./prml/*.tex; do echo "Convert $i"; ./articles/bin/mkhtml5 "$i" ;done; cd - || exit 1
 
